191453b0f98756ed96ac27c106b7fdbde5810059
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 16:10:36 +0100

    added function x generate random numbers

--- ex2/snacks2.js
+++ ex2/snacks2.js
@@ -4,3 +4,7 @@ const squadre = [
   { nome: "Inter", puntiFatti: 0, FalliSubiti: 0 },
   { nome: "Roma", puntiFatti: 0, FalliSubiti: 0 },
 ];
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
