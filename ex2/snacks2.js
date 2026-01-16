68608cbd47723b6d3ccef9092339ae7d2db3c7d0
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 16:29:19 +0100

    added function + console log

--- ex2/snacks2.js
+++ ex2/snacks2.js
@@ -1,22 +1,27 @@
-const squadre = [
-  { nome: "Milan", puntiFatti: 0, FalliSubiti: 0 },
-  { nome: "Juventus", puntiFatti: 0, FalliSubiti: 0 },
-  { nome: "Inter", puntiFatti: 0, FalliSubiti: 0 },
-  { nome: "Roma", puntiFatti: 0, FalliSubiti: 0 },
-];
+function eseuiSnack2() {
+  const squadre = [
+    { nome: "Milan", puntiFatti: 0, FalliSubiti: 0 },
+    { nome: "Juventus", puntiFatti: 0, FalliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, FalliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, FalliSubiti: 0 },
+  ];
 
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  for (let i = 0; i < squadre.length; i++) {
+    squadre[i].puntiFatti = randomInt(0, 50);
+    squadre[i].FalliSubiti = randomInt(0, 10);
+  }
+
+  const nomeEFalli = squadre.map((squadra) => {
+    return {
+      nome: squadra.nome,
+      FalliSubiti: squadra.FalliSubiti,
+    };
+  });
+
+  console.log("Snack 2 -> Array completo squadre:", squadre);
+  console.log("Snack 2 -> Nuovo array (nome e fall subiti):", FalliSubiti);
 }
-
-for (let i = 0; i < squadre.length; i++) {
-  squadre[i].puntiFatti = randomInt(0, 50);
-  squadre[i].FalliSubiti = randomInt(0, 10);
-}
-
-const nomeEFalli = squadre.map((squadra) => {
-  return {
-    nome: squadra.nome,
-    FalliSubiti: squadra.FalliSubiti,
-  };
-});
