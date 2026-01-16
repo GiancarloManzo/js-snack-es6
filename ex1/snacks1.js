dbd62e440b1f44af33c235600ad3ee3694b918f5
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 16:02:08 +0100

    fixed an error

--- ex1/snacks1.js
+++ ex1/snacks1.js
@@ -1,18 +1,20 @@
-const biciclette = [
-  { nome: "Bianchi", peso: 7.2 },
-  { nome: "Specialized", peso: 6.8 },
-  { nome: "Trek", peso: 7.0 },
-  { nome: "Cannondale", peso: 7.6 },
-  { nome: "Pinarello", peso: 6.6 },
-];
+function eseguiSnack1() {
+  const biciclette = [
+    { nome: "Bianchi", peso: 7.2 },
+    { nome: "Specialized", peso: 6.8 },
+    { nome: "Trek", peso: 7.0 },
+    { nome: "Cannondale", peso: 7.6 },
+    { nome: "Pinarello", peso: 6.6 },
+  ];
 
-let bicileggera = biciclette[0];
+  let bicileggera = biciclette[0];
 
-for (let i = 1; i < biciclette.length; i++) {
-  if (biciclette[i].peso < bicileggera.peso) {
-    bicileggera = biciclette[i];
+  for (let i = 1; i < biciclette.length; i++) {
+    if (biciclette[i].peso < bicileggera.peso) {
+      bicileggera = biciclette[i];
+    }
   }
+  console.log("La bici più leggera è:", bicileggera.nome),
+    "peso:",
+    bicileggera.peso;
 }
-console.log("La bici più leggera è:", bicileggera.nome),
-  "peso:",
-  bicileggera.peso;
