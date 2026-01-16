0b97a51d7f0cc558b2da70b876cbd2bb0d2cf7ab
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 15:44:36 +0100

    added btn x snacks esecution

---
+++ main.js
@@ -0,0 +1,10 @@
+const btnSnack1 = document.getElementById("btn-snack1");
+const btnSnack2 = document.getElementById("btn-snack2");
+
+btnSnack1.addEventListener("click", function () {
+  eseguiSnack1();
+});
+
+btnSnack2.addEventListener("click", function () {
+  eseguiSnack2();
+});
