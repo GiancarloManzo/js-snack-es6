ed2cb4db8a5262492ff4374e3b2bc0d7fdfe77c6
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 16:08:33 +0100

    added array of teams

---
+++ ex2/snacks2.js
@@ -0,0 +1,6 @@
+const squadre = [
+  { nome: "Milan", puntiFatti: 0, FalliSubiti: 0 },
+  { nome: "Juventus", puntiFatti: 0, FalliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, FalliSubiti: 0 },
+  { nome: "Roma", puntiFatti: 0, FalliSubiti: 0 },
+];
