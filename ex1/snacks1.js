22c060d4df64c9185185520764494c74d90e5c91
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 15:58:11 +0100

    added ciclo for + console log

--- ex1/snacks1.js
+++ ex1/snacks1.js
@@ -1,7 +1,18 @@
 const biciclette = [
   { nome: "Bianchi", peso: 7.2 },
-  { nome: "Bianchi", peso: 7.2 },
-  { nome: "Bianchi", peso: 7.2 },
-  { nome: "Bianchi", peso: 7.2 },
-  { nome: "Bianchi", peso: 7.2 },
+  { nome: "Specialized", peso: 6.8 },
+  { nome: "Trek", peso: 7.0 },
+  { nome: "Cannondale", peso: 7.6 },
+  { nome: "Pinarello", peso: 6.6 },
 ];
+
+let biciclette = biciclette[0];
+
+for (let i = 1; i < biciclette.length; i++) {
+  if (biciclette[i].peso < bicileggera.peso) {
+    bicileggera = biciclette[i];
+  }
+}
+console.log("La bici più leggera è:", bicileggera.nome),
+  "peso:",
+  bicileggera.peso;
