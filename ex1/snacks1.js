0775a1860ed7adb928c11b8c22f335173b84f36c
Giancarlo <cooper458.gm@gmail.com>
2026-01-16 15:50:36 +0100

    added array of object

---
+++ ex1/snacks1.js
@@ -0,0 +1,7 @@
+const biciclette = [
+  { nome: "Bianchi", peso: 7.2 },
+  { nome: "Bianchi", peso: 7.2 },
+  { nome: "Bianchi", peso: 7.2 },
+  { nome: "Bianchi", peso: 7.2 },
+  { nome: "Bianchi", peso: 7.2 },
+];
